40cf7f9895b2f771cf3f764916f344c1c9788d76
SpeedyKabal <62118414+SpeedyKabal@users.noreply.github.com>
2023-04-03 06:36:18 +0100

    Fix Bugs
    Delete Some Unused lines

--- GuessingtheNumber/main.py
+++ GuessingtheNumber/main.py
@@ -1,23 +1,24 @@
 import random as ran
-import math
 
 p = ran.randint(0, 30)
 
 print("The Computer Will choose a number between 0 and 30\ntry to Guess the number in less than 5 guesses")
 
-for guesscompt in range(0, 5):
+for guessCompt in range(0, 5):
     nbrguessed = int(input("Guess the Number : "))
+    if nbrguessed >= 0 and nbrguessed <= 30:
+        if nbrguessed > p:
+            print("Try Again! you guessed high")
 
-    if nbrguessed > p:
-        print("Try Again! you guessed high")
+        elif nbrguessed < p:
+            print("Try Again! you guessed small")
 
-    elif nbrguessed < p:
-        print("Try Again! you guessed small")
-
+        else:
+            break
     else:
-        break
+        print("Invalid Input")
 
 if nbrguessed == p:
-    print("You made it! you guess right\nNumber of guesses is: " + str(guesscompt + 1))
+    print("You made it! you guess right\nNumber of guesses is: " + str(guessCompt + 1))
 else:
-    print("You lose, The Chosen number is: " + str(p) + "\nNumber of guesses is: " + str(guesscompt + 1))
+    print("You lose, The Chosen number is: " + str(p) + "\nNumber of guesses is: " + str(guessCompt + 1))
